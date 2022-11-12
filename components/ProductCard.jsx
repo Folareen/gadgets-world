ac992b1ff75c5a91016ee190c01b411ace699c31
Folareen <sakawahab03@gmail.com>
2022-11-12 03:19:01 +0100

    adjusted productCard

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -1,22 +1,23 @@
 import { Box, Typography } from "@mui/material"
 import Link from 'next/link'
+import formatPrice from "../utils/formatPrice"
 
 const ProductCard = ({img_url, title, price, productId, categoryId}) => {
   return (
     <Link href={`/${categoryId}/${productId}`}>
-        <Box sx={{width: { xs: '200px', sm: '250px', lg: '300px'}, m: 2, boxShadow: '0 0 4px rgba(0, 0, 0, 0.3)', borderRadius: 2, '&:hover' :{
+        <Box sx={{width: { xs: '200px', sm: '250px', lg: '300px'}, m: 2, boxShadow: '0 0 4px rgba(0, 0, 0, 0.3)', borderRadius: 4, '&:hover' :{
             cursor: 'pointer'} }}>
         
-            <Box sx={{height: { xs: '150px', sm: '200px', lg: '250px'}, width: '100%', mb: 1}}>
-                <img src={img_url} style={{width: '100%', height: '100%', objectFit: 'cover'}} />
+            <Box sx={{height: { xs: '150px', sm: '200px', lg: '250px'}, width: '100%', mb: 1.5, borderRadius: 4, bgcolor: 'light.main', borderBottomLeftRadius: 0, borderBottomRightRadius: 0}}>
+                <img src={img_url} style={{width: '100%', height: '100%', objectFit: 'cover', borderTopRightRadius: 'inherit', borderTopLeftRadius: 'inherit'}} />
             </Box>
             
             <Typography sx={{fontSize: 18, color: 'primary.main', textTransform: 'capitalize'}}>
                 {title}
             </Typography>
 
-            <Typography sx={{color: 'dark.main', fontSize: 18}}>
-                ${price}
+            <Typography sx={{color: 'dark.main', fontSize: 16, my: 0.5}}>
+                ${formatPrice(price)}
             </Typography>
 
         </Box>
