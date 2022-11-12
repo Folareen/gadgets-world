aa3bc134c257239895d484d83aac57308f9c5bbd
Folareen <sakawahab03@gmail.com>
2022-11-12 21:30:18 +0100

    added product gallery

---
+++ components/ProductGallery.jsx
@@ -0,0 +1,37 @@
+import { Box } from "@mui/material"
+import { useState } from "react"
+import formatImageUrl from "../utils/formatImageUrl"
+
+const ProductGallery = ({images}) => {
+    const [imageInView, setImageInView] = useState(formatImageUrl(images[0].attributes.url))
+
+  return (
+    <Box sx={{ p: 2, width: {sx: '100%', md: '500px'}}}>
+
+        <Box sx={{width: '100%' , height: {xs: '300px', sm: '400px', md: '400px'}, mb: 1.5, borderRadius: 4, bgcolor: 'light.main'}}>
+            <img src={imageInView} style={{width: '100%', height: '100%', objectFit: 'cover', borderRadius: 'inherit'}} />
+        </Box>
+
+        <Box sx={{width: '100%', display: 'flex', justifyContent: 'space-between', maxWidth: '400px', mx: 'auto'}}>
+            {
+                images.map(
+                    ({attributes:{url}}) => {
+                        return (
+                            <Box onClick={() => setImageInView(formatImageUrl(url))} key={url} sx={{width: {xs: '70px', md: '80px'}, height: {xs: '70px', md: '80px'}, borderRadius: 2, opacity: (
+                                formatImageUrl(url) === imageInView ? 1 : 0.6
+                            ), '&:hover': {
+                                cursor: 'pointer', opacity: 1
+                            }}}>
+
+                                <img src={formatImageUrl(url)} style={{width: '100%', height: '100%', objectFit: 'cover', borderRadius: 'inherit'}}  />
+                            </Box>
+                        )
+                    }
+                )
+            }
+        </Box>
+    </Box>
+  )
+}
+
+export default ProductGallery
