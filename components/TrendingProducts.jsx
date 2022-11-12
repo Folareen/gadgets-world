5b4be39883c37d7cd2a55fbc3d6819516df13627
Folareen <sakawahab03@gmail.com>
2022-11-12 03:20:03 +0100

    added trendingProducts section

---
+++ components/TrendingProducts.jsx
@@ -0,0 +1,39 @@
+import { Box, Typography } from "@mui/material"
+import ProductCard from "./ProductCard"
+
+const TrendingProducts = ({trendingProducts}) => {
+  return (
+    <Box sx={{textAlign: 'center', mt: 5}}>
+
+        <Typography sx={{fontSize: 26, color: 'dark.main'}}>
+            Trending Gadgets
+        </Typography>
+
+        {
+            trendingProducts.map(
+                ({attributes}) => {
+                    return(
+                        <>
+                            <Typography sx={{fontSize: 20, mt: 2, textTransform: 'capitalize', color: 'dark.main' }}>
+                                {attributes.name}
+                            </Typography>
+                            <Box sx={{display: 'flex', justifyContent: 'center', flexWrap: 'wrap'}}>
+                            {
+                                attributes.products.data.slice(0, 3).map(
+                                    ({id, attributes: {title, price, images }}) => {
+                                        return <ProductCard img_url={`http://localhost:1337${images.data[0].attributes.url}`} title={title} price={price}  productId={id} categoryId={attributes.name} key={id} />
+                                    }
+                                )
+                            }
+                            </Box>
+                        </>
+                    )
+                }
+            )
+        }
+
+    </Box>
+  )
+}
+
+export default TrendingProducts
