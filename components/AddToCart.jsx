6c46f79ca92d8170181b8b9373cd5c8f22c8e20e
Folareen <sakawahab03@gmail.com>
2022-11-13 22:12:38 +0100

    done with 'add product to cart'

--- components/AddToCart.jsx
+++ components/AddToCart.jsx
@@ -5,10 +5,35 @@ import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
 import AddShoppingCartRoundedIcon from '@mui/icons-material/AddShoppingCartRounded';
 import ShoppingBagRoundedIcon from '@mui/icons-material/ShoppingBagRounded';
 import {useRouter} from "next/router";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import { addProduct, removeProduct } from "../features/cartSlice";
 
-const AddToCart = () => {
-    const [quantity, setQuantity] = useState(0)
+const AddToCart = ({image, title, price, productId}) => {
+    const state = useSelector(state => state.cart)
+
+    const [quantity, setQuantity] = useState(state.products.find(
+        (product) => product.productId === productId
+    )?.quantity || 1)
+
     const {push} = useRouter()
+    const dispatch = useDispatch()
+
+    const addToCart = () => {
+        dispatch(addProduct({
+            productDetails : {productId, image, title, price, quantity} , state
+        }))
+        toast.success(`${quantity} ${title} added to cart.`)
+    }
+
+    const removeFromCart = () => {
+
+        dispatch(removeProduct({
+            productId, state
+        }))
+        toast.error(`${title} removed from cart.`)
+
+    }
 
   return (
     <Box >
@@ -21,7 +46,7 @@ const AddToCart = () => {
                 {quantity}
             </Typography>
             <Button onClick={() => {
-                if(quantity > 0) setQuantity(quantity - 1 )
+                if(quantity > 1) setQuantity(quantity - 1 )
             }} color="error">
                 <RemoveRoundedIcon />
             </Button>
@@ -35,9 +60,10 @@ const AddToCart = () => {
                 Buy now <ShoppingBagRoundedIcon fontSize="small" sx={{mx: 1}}/>
             </Button>
 
-            <Button color="success" variant='outlined' sx={{borderRadius: 2, m: 1, px: 2}}>
+            <Button color="success" variant='outlined' sx={{borderRadius: 2, m: 1, px: 2}} onClick={addToCart}>
                 Add to cart <AddShoppingCartRoundedIcon fontSize="small" sx={{mx: 1}} />
             </Button>
+
         </Box>
 
     </Box>
