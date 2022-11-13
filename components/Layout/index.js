fdd45dd19d5f8ecd39d429f381287277e843eac4
Folareen <sakawahab03@gmail.com>
2022-11-13 02:32:15 +0100

    added firebase auth persistence

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,10 +2,29 @@ import Head from "next/head";
 import Footer from "./Footer";
 import Header from "./Header";
 import { Box } from "@mui/material";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { auth } from "../../firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { authenticated, authenticating } from "../../features/userSlice";
+import { useEffect } from "react";
 
 const Layout = ({ children }) => {
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      dispatch(authenticating());
+      if (user) {
+        dispatch(authenticated(user.providerData[0]));
+      } else {
+        dispatch(authenticated(null));
+      }
+    });
+  }, [data]);
+
   return (
     <Box
       sx={{
