c8205d0ee79ef610fa4aaf647337825b186da74b
Folareen <sakawahab03@gmail.com>
2022-11-14 12:36:52 +0100

    done with 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,49 @@
+import { Typography, Box, Button } from "@mui/material";
 import Link from "next/link";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import ArrowForwardRoundedIcon from "@mui/icons-material/ArrowForwardRounded";
+
 const NotFound = () => {
   return (
-    <div>
-      404
-      <Link href="/">Home</Link>
-    </div>
+    <Box
+      sx={{
+        height: "100vh",
+        width: "100%",
+        position: "absolute",
+        top: 0,
+        right: 0,
+        left: 0,
+        bottom: 0,
+        zIndex: 1,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box sx={{ width: "max-content", textAlign: "center" }}>
+        <Typography
+          sx={{
+            fontSize: 20,
+            color: "dark.main",
+            my: 2,
+          }}
+        >
+          404...Page not found.
+        </Typography>
+        <Link href="/">
+          <Button
+            sx={{
+              my: 2,
+              px: 4,
+            }}
+            variant={"contained"}
+          >
+            <Typography>Go Home</Typography>
+            <HomeRoundedIcon fontSize="small" sx={{ ml: 1 }} />
+          </Button>
+        </Link>
+      </Box>
+    </Box>
   );
 };
 
