b50285413b8c3cfecfcbdb6d8e3eb8b2aad26c14
Folareen <sakawahab03@gmail.com>
2022-11-14 02:07:25 +0100

    changed color

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -3,10 +3,10 @@ import { Typography, Box, Link } from "@mui/material"
 const Footer = () => {
   return (
     <Box component='footer' sx={{position: 'absolute', bottom: 1, textAlign: 'center', width: '100%', borderTopWidth: 1, borderTopStyle: 'solid', borderTopColor: 'dark.main'}}>
-    <Typography >
+    <Typography sx={{color: 'dark.main'}} >
         Gadgets World &copy; 2022
     </Typography>
-    <Typography>
+    <Typography sx={{color: 'dark.main'}}>
         Built by 
         <Link href="https://twitter.com/_folareen_" sx={{ml: 0.5}}>
             <span>
